84baa71390189bf4c1b0009f2b342454b30079f9
AkoKBIkeda <akikoi1024@gmail.com>
2025-08-25 03:11:03 +1200

    for deployment

--- jobsearch/settings.py
+++ jobsearch/settings.py
@@ -90,8 +90,7 @@ DATABASES = {
 
 if os.environ.get("DATABASE_URL"):
     DATABASES["default"] = dj_database_url.config(
-        default=os.environ["DATABASE_URL"], conn_max_age=600, 
-        ssl_require=True),
+        default=os.environ["DATABASE_URL"], conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
